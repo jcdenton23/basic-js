67871ef73d6376f48c4959eac39692c3f8b2404d
jcdenton23 <fifaxjcd23@gmail.com>
2021-09-29 02:27:28 +0600

    st-mine-sweeper.js

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,49 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+  const resMatrix = new Array(matrix.length)
+    .fill(0)
+    .map(() => new Array(3).fill(0));
+
+  for (let col = 0; col < matrix.length; col++) {
+    for (let row = 0; row < matrix[col].length; row++) {
+      let counter = 0;
+
+      if (row > 0) {
+        if (matrix[col][row - 1]) counter++;
+      }
+
+      if (row !== matrix.length - 1) {
+        if (matrix[col][row + 1]) counter++;
+      }
+
+      if (col > 0) {
+        if (matrix[col - 1][row]) counter++;
+      }
+
+      if (col < matrix.length - 1) {
+        if (matrix[col + 1][row]) counter++;
+      }
+
+      if (col > 0 && row > 0) {
+        if (matrix[col - 1][row - 1]) counter++;
+      }
+
+      if (col > 0 && row !== matrix.length - 1) {
+        if (matrix[col - 1][row + 1]) counter++;
+      }
+
+      if (col < matrix.length - 1 && row !== matrix.length - 1) {
+        if (matrix[col + 1][row + 1]) counter++;
+      }
+
+      if (col < matrix.length - 1 && row > 0) {
+        if (matrix[col + 1][row - 1]) counter++;
+      }
+
+      resMatrix[col][row] = counter;
+    }
+  }
+  return resMatrix;
 }
