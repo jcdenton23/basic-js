66afb98d5f416e983e2d0010943e1d121622732e
jcdenton23 <fifaxjcd23@gmail.com>
2021-09-29 04:02:00 +0600

    vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,29 +3,83 @@ import { NotImplementedError } from '../extensions/index.js';
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getKey(str, key) {
+    const kf = Math.ceil(str.length / key.length);
+    return key.repeat(kf).slice(0, str.length).toUpperCase();
+  }
+
+  encrypt(str, key) {
+    if (!str || !key) throw new Error('Incorrect arguments!');
+    key = this.getKey(str, key);
+    str = str.toUpperCase();
+
+    const startPos = 'A'.charCodeAt(0);
+    const endPos = 'Z'.charCodeAt(0);
+    const abcCount = 26;
+
+    const res = [];
+
+    for (let i = 0, j = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) >= startPos && str.charCodeAt(i) <= endPos) {
+        let letterIdx = str.charCodeAt(i) - startPos;
+        let shift = key.charCodeAt(j) - startPos;
+        res.push(
+          String.fromCharCode(startPos + ((letterIdx + shift) % abcCount))
+        );
+        j++;
+      } else {
+        res.push(str[i]);
+      }
+    }
+    return this.type ? res.join('') : res.reverse().join('');
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error('Incorrect arguments!');
+    key = this.getKey(str, key);
+    str = str.toUpperCase();
+
+    const startPos = 'A'.charCodeAt(0);
+    const endPos = 'Z'.charCodeAt(0);
+    const abcCount = 26;
+
+    const res = [];
+
+    for (let i = 0, j = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) >= startPos && str.charCodeAt(i) <= endPos) {
+        let letterIdx = str.charCodeAt(i) - startPos;
+        let shift = key.charCodeAt(j) - startPos;
+        res.push(
+          String.fromCharCode(
+            startPos + ((letterIdx - shift + abcCount) % abcCount)
+          )
+        );
+        j++;
+      } else {
+        res.push(str[i]);
+      }
+    }
+    return this.type ? res.join('') : res.reverse().join('');
   }
 }
